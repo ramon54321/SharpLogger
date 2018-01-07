878b2ac71a1c445144ca0235d4dbe7f7ef466f55
Ramon Brand <ramon2009@gmail.com>
2018-01-07 13:54:51 +0200

    Started to add examples.

---
+++ examples/ExampleLoggerUsage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace SharpLogger.Examples
+{
+    public class ExampleLoggerUsage
+    {
+        public ExampleLoggerUsage()
+        {
+            // -- Create a new object of this type and view the output.
+
+            // -- Set up the logger
+            // TODO: Write example usage
+            //Logger.SetPrinter(new )
+
+
+        }
+    }
+
+    class ConsolePrinter : Printer
+    {
+        public override void Print(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
